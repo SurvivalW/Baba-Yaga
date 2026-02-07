b44f756281548695d8aee3b23b5d7ef382db5ba3
subixsh04 <subikshasvaidhyanathan@gmail.com>
2026-02-06 19:48:52 -0500

    trying to access data from csv file

--- App/db/db.py
+++ App/db/db.py
@@ -11,3 +11,23 @@ conn = mysql.connector.connect(
 )
 
 cursor = conn.cursor()
+
+DATA_FOLDER = os.path.join(os.getcwd(), "MakingData")
+
+store_files = {
+    1: "Kroger.csv",
+    2: "Meijer.csv",
+    3: "Sam's Club.csv",
+    4: "Target.csv",
+    5: "Walmart.csv"
+}
+
+#inserting values into stores
+for store_id, filename in store_files.items():
+    store_name = filename.replace(".csv", "")
+    cursor.execute(
+        "INSERT IGNORE INTO stores (store_id, store_name) VALUES (%s, %s)",
+        (store_id, store_name)
+    )
+
+conn.commit()
